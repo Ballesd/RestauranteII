9fb8d52a7894f7e580f16bbb880f0b7e3c8500e5
Ballesd <ballesterosj500@gmail.com>
2022-09-06 14:54:53 -0500

    Inicialización del front del proyecto con vue

---
+++ RestFront/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import {ref} from 'vue';
+
+  const numero = ref(0);
+
+  const aumentarNumero = () =>{
+    numero.value++; 
+  }
+
+</script>
+
+<template>
+  <h1>Hola</h1>
+  <p>{{numero}}</p>
+  <button @click="aumentarNumero">Aumentar</button>
+</template>
+
+<style scoped>
+
+</style>
+
